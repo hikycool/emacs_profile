db7bcb3c4ff593221cce1e432738f4675dd7b95d
root <root@syu-devstack.novalocal>
2017-06-28 06:21:16 +0000

    BUG FIXed
    BY Yu

--- emacs.d/.emacs.d/elpa/eww-lnum-20150102.712/eww-lnum-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "eww-lnum" "20150102.712" "Conkeror-like functionality for eww" 'nil :url "https://github.com/m00natic/eww-lnum" :keywords '("eww" "browse" "conkeror"))

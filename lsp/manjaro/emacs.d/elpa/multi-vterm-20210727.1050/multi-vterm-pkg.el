a29c37b27fd605a8e67363ea0579724dcbc067f1
yushengzuo <466427645@qq.com>
2022-03-01 23:14:15 +0800

    feature: promote macos version and manjaro version

---
+++ lsp/manjaro/emacs.d/elpa/multi-vterm-20210727.1050/multi-vterm-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from multi-vterm.el  -*- no-byte-compile: t -*-
+(define-package "multi-vterm" "20210727.1050" "Like multi-term.el but for vterm" '((emacs "26.3") (vterm "0.0") (project "0.3.0")) :commit "a3df7218c1ecadef779e2c47815201052283f9ea" :keywords '("terminals" "processes") :url "https://github.com/suonlight/multi-libvterm")
